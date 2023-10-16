3d1abf0a0b7a00da06424924e660d55b66418da4
Mehul Gupta <mehulgupta2110@gmail.com>
2023-10-16 07:37:43 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,32 @@
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  Button,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import StackNavigator from './StackNavigator';
+
+function App() {
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
+}
+
+export default App;
